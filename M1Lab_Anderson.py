3ef48a3db3b824b9ea3a42d04fd46eaeabac7122
Doogie63 <31226802+Doogie63@users.noreply.github.com>
2019-01-23 12:00:20 -0500

    Add files via upload

---
+++ M1Lab_Anderson.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+CSC 221
+M1LAB
+Todd Anderson
+23 Jan 2018
+"""
+def main():
+    """
+    Bottles of beer song
+    """
+    for beer in range(99, 0, -1):
+     print(beer, "beers on the wall")
+     print("take on down pass it around")
+     
+     if beer == 1:
+         print("no more beers left")
+    
+    
+ 
+
+main()
